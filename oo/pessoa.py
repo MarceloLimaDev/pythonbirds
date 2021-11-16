1737e53070dda541c036cec0cff7d8ada44ca861
marcelo <marcelo_lima_dh@hotmail.com>
2021-11-15 21:14:14 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=28):
+    def __init__(self, *filhos, nome=None, idade=28):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Camila')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Marcelo'
-    print(p.nome)
-    print(p.idade)
-
-
-
+    marcelo = Pessoa(nome='marcelo')
+    camila = Pessoa(marcelo, nome='camila')
+    print(Pessoa.cumprimentar(camila))
+    print(id(camila))
+    print(camila.cumprimentar())
+    print(camila.nome)
+    print(camila.idade)
+    for filho in camila.filhos:
+        print(filho.nome)
+    camila.sobrenome = 'lima'
+    del camila.filhos
+    print(camila.__dict__)
+    print(marcelo.__dict__)
